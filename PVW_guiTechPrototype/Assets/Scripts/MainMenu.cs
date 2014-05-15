a1f13a693ee536a6379b58187197a0c78b535ba5
Dardlem <burmistrovartyom@gmail.com>
2014-05-15 19:26:15 +0200

    Basic project files.
    PVW basic project.

---
+++ PVW_guiTechPrototype/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	Simulation m_Simulation = null;
+
+	void Start()
+	{
+		GameObject scriptHolder = GameObject.Find("ScriptHolder");
+
+		m_Simulation = scriptHolder.GetComponent<Simulation>();
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Box("Main Menu");
+
+		if (GUILayout.Button ("Start simulation"))
+						m_Simulation.StartSimulation();
+	}
+}
