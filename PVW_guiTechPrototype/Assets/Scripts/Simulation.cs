4cade85fe18a95f3b9f0ac4079810802d12742cc
Dardlem <burmistrovartyom@gmail.com>
2014-05-15 19:35:56 +0200

    Basic project files.
    Project Virtual World.

---
+++ PVW_guiTechPrototype/Assets/Scripts/Simulation.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Simulation : MonoBehaviour {
+
+	public void StartSimulation()
+	{
+		Debug.Log ("Start");
+	}
+
+	void Step()
+	{}
+}
